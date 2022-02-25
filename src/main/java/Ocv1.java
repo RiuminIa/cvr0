4f8337d4b886a0c84264bfcb4921567dff0b2634
Riuminjake <64644764+Riuminjake@users.noreply.github.com>
2022-02-25 20:45:03 +0300

    kjhkhkhjkhj

--- src/main/java/Ocv1.java
+++ src/main/java/Ocv1.java
@@ -1,6 +1,11 @@
 public class Ocv1 {
     public static void main(String[] args) {
         System.out.println("hhhhiiiii");
-        System.out.println("hhhhiidfiii");
+        System.out.println("hhhhiiiii");
+        System.out.println("zai");
+        if(0==0){
+            System.out.println("zakjhkhjika");
+        }
+
     }
 }
